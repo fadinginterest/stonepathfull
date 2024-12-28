ca73b9100ab12d32b71d10787f1d06241f2224df
fadinginterest <fadinginterest@users.noreply.github.com>
2024-12-27 22:38:48 -0500

    Created new shapes for Stonepath and Stonepath Slab to correct their voxel heights.

---
+++ stonepathfull/stonepathfullModSystem.cs
@@ -0,0 +1,27 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace stonepathfull
+{
+    public class stonepathfullModSystem : ModSystem
+    {
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            api.Logger.Notification("Hello from stonepathfull mod: " + api.Side);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Logger.Notification("Hello from stonepathfull mod server side: " + Lang.Get("stonepathfull:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            api.Logger.Notification("Hello from stonepathfull mod client side: " + Lang.Get("stonepathfull:hello"));
+        }
+    }
+}
